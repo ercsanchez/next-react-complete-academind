f83c838d6793ecd40d28c534f18d4c0648bdb1c5
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-04-25 04:52:16 +0800

    14-12 use Link comp

--- 14-summary/src/pages/news/index.js
+++ 14-summary/src/pages/news/index.js
@@ -1,5 +1,20 @@
+import Link from 'next/link';
+
 function News() {
-  return <h1>News Page</h1>;
+  return (
+    <>
+      <h1>News Page</h1>
+      {/* news posts */}
+      <ul>
+        <li>
+          <Link href="/news/news-post-1">News Post 1</Link>
+        </li>
+        <li>
+          <Link href="/news/something-else">Something else</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
 
 export default News;
