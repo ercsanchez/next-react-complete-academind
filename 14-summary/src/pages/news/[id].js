6aaa1b79e15c8c3c94223e1c51b3e23e672cebf7
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-04-25 04:28:36 +0800

    14-11 extract dynamic page's url/path/route

--- 14-summary/src/pages/news/[id].js
+++ 14-summary/src/pages/news/[id].js
@@ -1,5 +1,14 @@
+import { useRouter } from 'next/router';
+
 function DetailPage() {
-  return <h1>Detail Page</h1>;
+  const router = useRouter();
+  const newsId = router.query.id;
+  return (
+    <>
+      <h1>Detail Page</h1>
+      <p>News ID: {newsId}</p>
+    </>
+  );
 }
 
 export default DetailPage;
