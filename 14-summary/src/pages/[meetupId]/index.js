fb6eddd7d4a7197182166b0aaefe67e24330b2b8
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-04-27 21:46:36 +0800

    14-24 use SSG for /meetupId page (meetup detail)

--- 14-summary/src/pages/[meetupId]/index.js
+++ 14-summary/src/pages/[meetupId]/index.js
@@ -1,12 +1,35 @@
 import MeetupDetails from '../../../components/meetups/MeetupDetails';
 
-export default function MeetupDetailsPage() {
+export default function MeetupDetailsPage({ meetupData }) {
+  const { image, title, address, description } = meetupData;
   return (
     <MeetupDetails
-      image="https://upload.wikimedia.org/wikipedia/commons/8/8a/Magellan%27s_Cross_full.jpg"
-      title="1st meetup"
-      address="magellan's cross"
-      description="The meetup description"
+      image={image}
+      title={title}
+      address={address}
+      description={description}
     />
   );
+}
+
+export async function getStaticProps(context) {
+  // fetch data from an API / DB calls / Filesystem operations
+
+  const meetupId = context.params.meetupId;
+
+  // only logs in development server when successfully navigating to dynamic path
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/8/8a/Magellan%27s_Cross_full.jpg',
+        title: '1st meetup',
+        address: "magellan's cross",
+        description: 'The meetup description',
+      },
+    },
+  };
 }
