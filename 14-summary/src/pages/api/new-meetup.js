ac3c6ff86735759a357cef7f9bebb4f2bddfcdd6
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-04-28 14:09:11 +0800

    14-27 test querying all meetups for route: /api/new-meetups

--- 14-summary/src/pages/api/new-meetup.js
+++ 14-summary/src/pages/api/new-meetup.js
@@ -1,11 +1,18 @@
 // /api/new-meetup
+import prisma from '../../../lib/prisma';
 
-function handler(req, res) {
+async function handler(req, res) {
   // req.method, req.body, req.headers
   if (req.method === 'POST') {
     const data = req.body;
 
     const { title, image, address, description } = data;
+  }
+
+  // test querying db
+  if (req.method === 'GET') {
+    let meetups = await prisma.meetup.findMany();
+    return res.json({ data: meetups });
   }
 }
 
